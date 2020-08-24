37c2d4a46bba72748e97eac8792e6f9dbd4489af
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2020-08-24 15:56:41 +0300

    added passport-jwt authentication for post route in blogController

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,7 +1,33 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
 const router = require('express').Router()
 const Post = require('../models/postModel')
+const User = require('../models/userModel')
 const Comment = require('../models/postModel')
 
+//Initializing passport
+
+//Creating an object of options to pass it JwtStrategy
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = process.env.SECRET
+
+passport.use(new JwtStrategy(opts,(payload,done)=>{
+    console.log('i got a dui baaby')
+    User.findById(payload.id,(err,user)=>{
+        if(err){
+            return done(err,false)
+        }
+        if(user){
+            return done(null,user)
+        }else{
+            return done(null,false)
+        }
+    })
+}))
 
 //GET request to get all blogs
 
@@ -26,7 +52,7 @@ router.get('/posts/:id',(req,res)=>{
 
 //POST request 
 
-router.post('/posts',(req,res)=>{
+router.post('/posts',passport.authenticate('jwt',{session:false}),(req,res)=>{
     
     let post = new Post({
         title:req.body.title,
